32f3bdad4c76057cd4b8d222e6754dd404d665ee
alexghz81 <alex-ghz@yandex.ru>
2022-05-31 10:58:10 +0300

    add api for swapi

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-uzojogvoj5r">
+      <div className="ldio-sj5aeuxvr5">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner;
